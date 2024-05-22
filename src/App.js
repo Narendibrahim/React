c17ac3793587cdbbe0515c719e4c8fc72d88a8fd
Narendibrahim <162007956+Narendibrahim@users.noreply.github.com>
2024-05-23 00:11:08 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, I'm - Narend!</h1>
+        <p>This year, I'm turning 19 years old and I live in South Tangerang.</p>
+        <p>My full name is Narendra Ibrahim Nurtaqiy and I am a Digital Business student at Padjadjaran University.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
